d2c8ec570fff7a6d32aefd9b74e7fe2aa16d845d
rockage <122590@qq.com>
2021-11-27 02:34:46 +0800

    update

--- src/components/product_view.vue
+++ src/components/product_view.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="main">
-    <v-carousel hide-delimiters v-if="carousel">
-      <v-carousel-item
-        v-for="(item, i) in pic_group"
-        :key="i"
-        :src="item.src"
-        reverse-transition="fade-transition"
-        transition="fade-transition"
-      ></v-carousel-item>
+    <v-carousel>
+      <v-carousel-item v-for="(item, i) in pic_group" :key="i">
+        <div
+          style="
+            text-align: center;
+            width: 100%;
+            height: 100%;
+            background: white;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          "
+        >
+          <img :src="item.src" style="max-width: 640px;max-height: 500px" />
+        </div>
+      </v-carousel-item>
     </v-carousel>
 
     <div v-html="output" class="html_output"></div>
   </div>
 </template>
 <script>
-
 export default {
   name: "products",
   data() {
@@ -22,6 +30,7 @@ export default {
       carousel: false,
       output: "",
       pic_group: [],
+      colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
     };
   },
   computed: {},
@@ -36,8 +45,8 @@ export default {
       this.axios.post("productContents", param).then((response) => {
         if (response.data) {
           me.carousel = true;
-          let data = JSON.parse(response.data)
-          me.output = data[0].contents
+          let data = JSON.parse(response.data);
+          me.output = data[0].contents;
           for (let i = 1; i <= 5; i++) {
             me.pic_group.push({
               src:
@@ -56,13 +65,6 @@ export default {
 
 
 <style scoped>
-img {
-  max-width: 800px;
-  display: block;
-}
-
-
-
 .h1-text {
   text-align: center;
   margin-top: 10px;
@@ -70,9 +72,9 @@ img {
 
 .html_output {
   line-height: 2 !important;
-  white-space:pre-wrap;
+  white-space: pre-wrap;
   font-family: "Lato", sans-serif;
-  margin-top:20px;
+  margin-top: 20px;
 }
 
 .title {
